0b0f742a3dea84e141a92401530b3802482c0446
Juanjo Torres <jjtr96@gmail.com>
2021-03-18 21:39:46 +0100

    Debugging config.php

--- models/config.php
+++ models/config.php
@@ -1,8 +1,8 @@
 <?php
     $db['default'] = array(
         'hostname' => 'eu-cdbr-west-03.cleardb.net',
-        'username' => 'b36cb079579033',
-        'password' => '90ac0673',
-        'database' => 'heroku_382cd94c0de8332'
+        'username' => 'b5abad99bff3f4',
+        'password' => '797316dc',
+        'database' => 'heroku_886b29e3cb7edc3'
     );
 ?>
